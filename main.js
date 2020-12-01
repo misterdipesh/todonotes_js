d900e36bcd977a8ca31e7502dc6e967b9046efb2
Dipesh Bishwakarma <deepeesh471@gmail.com>
2020-12-01 15:23:26 +0545

    second commit

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 let mynote_key='my_note_key';
+let my_notes=[];
 window.onload=function(){
 	getSavedNotes();
 }
 function onAddNote(){
-let myNote=document.getElementById('note').value;//gets the value of textara note
-localStorage.setItem(mynote_key,myNote);
-getSavedNotes();
+	let myNote=document.getElementById('note').value;
+	let note={};
+	note.value=myNote;
+	note.date=new Date();
+	my_notes.push(note);
+
+	localStorage.setItem(mynote_key,JSON.stringify(my_notes));
+	
+	getSavedNotes();
 }
 function getSavedNotes(){
 	let notes=localStorage.getItem(mynote_key);
-	document.getElementById('savedNotes').innerText=notes;
+	let my_notes_html='';
+
+	
+	JSON.parse(notes).forEach(function(val){
+	my_notes_html=my_notes_html+
+	'<div> <p>'+val.date+'\n</p><p>'+val.value+'\n</p></div>'
+	});
+	document.getElementById('savedNotes').innerHTML=my_notes_html;
 }
